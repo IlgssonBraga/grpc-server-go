98ef5a508e7cc07d386d7a592f2de8033045857d
Ilgsson Braga <ilgssonbraga@localhost.localdomain>
2021-03-01 12:24:33 -0300

    feat: client stream

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/IlgssonBraga/grpc-go/pb"
 	"google.golang.org/grpc"
@@ -22,7 +23,9 @@ func main()  {
 	client := pb.NewUserServiceClient(connection)
 
 	// AddUser(client)
-	AddUserVerbose(client)
+	// AddUserVerbose(client)
+
+	AddUsers(client)
 }
 
 func AddUser(client pb.UserServiceClient)  {
@@ -66,4 +69,58 @@ func AddUserVerbose(client pb.UserServiceClient) {
 		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
 	}
 
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id: "i1",
+			Name: "Ilgsson",
+			Email: "ilgsson@gmail.com",
+		},
+		&pb.User{
+			Id: "i1",
+			Name: "Ilgsson",
+			Email: "ilgsson@gmail.com",
+		},
+		&pb.User{
+			Id: "i2",
+			Name: "Ilgsson2",
+			Email: "ilgsson2@gmail.com",
+		},
+		&pb.User{
+			Id: "i3",
+			Name: "Ilgsson3",
+			Email: "ilgsson3@gmail.com",
+		},
+		&pb.User{
+			Id: "i4",
+			Name: "Ilgsson4",
+			Email: "ilgsson4@gmail.com",
+		},
+		&pb.User{
+			Id: "i5",
+			Name: "Ilgsson5",
+			Email: "ilgsson5@gmail.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+
+	if err != nil {
+		log.Fatalf("Erro creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+
+	if err != nil {
+		log.Fatalf("Erro receiving response: %v", err)
+	}
+
+	fmt.Println(res)
 }
